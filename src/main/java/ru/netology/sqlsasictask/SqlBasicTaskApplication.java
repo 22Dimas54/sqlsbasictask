03564902c4317726d3f364a7b1c6261a07979d7c
Dmitry Ushakov <ushakov-dp@mail.ru>
2022-11-15 21:02:20 +0700

    dao add files

---
+++ src/main/java/ru/netology/sqlsasictask/SqlBasicTaskApplication.java
@@ -0,0 +1,24 @@
+package ru.netology.sqlsasictask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SqlBasicTaskApplication {
+
+    private UserService service = new UserService();
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlBasicTaskApplication.class, args);
+    }
+
+    @GetMapping("/products/fetch-product")
+    public void hello(@RequestParam("name") String name) {
+        service.handler(name);
+    }
+
+}
