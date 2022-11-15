4effa3d69acd937b1e38ad871c6e3f23b945df8e
Dimas <40695391+22Dimas54@users.noreply.github.com>
2022-11-15 21:03:24 +0700

    Delete 1.sql

--- 1.sql
+++
@@ -1,8 +0,0 @@
-create table netology.PERSONS(
-    name varchar not null ,
-    surname varchar not null ,
-    age int check ( age>0 ) default 1,
-    phone_number int,
-    city_of_living varchar default 'Novosibirsk',
-    PRIMARY KEY (name,surname, age)
-)
